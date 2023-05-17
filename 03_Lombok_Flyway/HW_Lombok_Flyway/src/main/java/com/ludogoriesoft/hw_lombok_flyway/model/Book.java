394eacc2e7600679ae65398b3253615063a29ba2
civelin <tsvetelinaivelin@gmail.com>
2023-05-17 22:53:39 +0300

    created books table

---
+++ 03_Lombok_Flyway/HW_Lombok_Flyway/src/main/java/com/ludogoriesoft/hw_lombok_flyway/model/Book.java
@@ -0,0 +1,21 @@
+package com.ludogoriesoft.hw_lombok_flyway.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table(name = "books")
+public class Book {
+    @Id
+    private Long id;
+
+    private String title;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id")
+    private Author author;
+
+}
