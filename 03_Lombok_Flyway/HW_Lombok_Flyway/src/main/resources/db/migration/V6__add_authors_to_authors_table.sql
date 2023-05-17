b1eca0779f47e9e2e761404d5c4b2bf06c17df3e
civelin <tsvetelinaivelin@gmail.com>
2023-05-18 00:21:30 +0300

    added authors to authors table

---
+++ 03_Lombok_Flyway/HW_Lombok_Flyway/src/main/resources/db/migration/V6__add_authors_to_authors_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO authors(first_name, last_name, birth_year)
+VALUES ('Stephen', 'King', 1947);
+INSERT INTO authors(first_name, last_name, birth_year)
+VALUES ('Bernard', 'Werber', 1961);
