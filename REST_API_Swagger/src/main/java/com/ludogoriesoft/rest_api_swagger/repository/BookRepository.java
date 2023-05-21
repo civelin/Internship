dae3ca306f0ea6d852d58cd8b3971e25dae9ff38
civelin <tsvetelinaivelin@gmail.com>
2023-05-21 15:53:34 +0300

    changed REST_API_Swagger folder

--- REST_API_Swagger/src/main/java/com/ludogoriesoft/rest_api_swagger/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ludogoriesoft.rest_api_swagger.repository;
-
-import com.ludogoriesoft.rest_api_swagger.entity.Book;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface BookRepository extends JpaRepository<Book, Long> {
-}
